f9ad85c46bbe83ee46cb812c2a2b8daf90a0df70
Apostolidis Christos <it2021010@hua.gr>
2026-09-11 21:18:53 +0300

    Fixed config for dev run

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
   }
 })
